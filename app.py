21bf1a265efdd4789c2dbfb7794af84e5af33d56
Srirammkm <35733243+Srirammkm@users.noreply.github.com>
2021-02-15 09:22:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def hello():
-        return render_template("index.html")
-
-    if __name__=='__main__':
-            app.run()
+    return render_template("index.html")
+if __name__=='__main__':
+    app.run()
